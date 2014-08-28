89b5ffbc8237fb7738e2aad56da5ff11b6ad623d
whitehaven <accrist@me.com>
2014-08-28 00:55:05 -0600

    Working and with -h help.

--- WordlyCount/main.cpp
+++ WordlyCount/main.cpp
@@ -38,13 +38,38 @@ void stripInput( string & subject )
 
 int main(int argc, const char * argv[])
 {
+    if ( !strncmp(argv[1], "-h", 2 ) )
+    {
+        cout << endl;
+        cout << "          WordlyCount v1.1             " << endl;
+        cout << "|-------------------------------------|" << endl;
+        cout << "|   Counts occurences of words.       |" << endl;
+        cout << "|                                     |" << endl;
+        cout << "|  Format: ./WordlyCount [opt] file   |" << endl;
+        cout << "|                                     |" << endl;
+        cout << "| -h : show this help                 |" << endl;
+        cout << "|                                     |" << endl;
+        cout << "| -g : use Greater Than 1 Only Mode   |" << endl;
+        cout << "|      (doesn't keep any word only    |" << endl;
+        cout << "|       shown once.)                  |" << endl;
+        cout << "|                                     |" << endl;
+        cout << "|   File can have any punctuation.    |" << endl;
+        cout << "|   Possesive case is also supported. |" << endl;
+        cout << "|                                     |" << endl;
+        cout << "|          GPLv3 Lisence              |" << endl;
+        cout << "| Copyright WhiteHaven informatics    |" << endl;
+        cout << "|_____________________________________|" << endl;
+
+        return 0;
+    }
+    
     bool useGreaterThan1OnlyMode = false;
     
     ifstream fin;
     
+
     if ( !strncmp(argv[1], "-g", 2 ) )
     {
-        cout << "Using greater-than-1 mode." << endl;
         useGreaterThan1OnlyMode = true;
         fin.open( argv[2] );
     }
@@ -73,7 +98,7 @@ int main(int argc, const char * argv[])
 
     for ( map<string, int>::iterator itermap = words.begin(); itermap != words.end(); itermap++ )
     {
-        if ( (useGreaterThan1OnlyMode && itermap->second > 1) )
+        if ( (useGreaterThan1OnlyMode && itermap->second > 1) || !useGreaterThan1OnlyMode )
         {
             currentEntry.first = itermap->second;
             currentEntry.second = itermap->first;
