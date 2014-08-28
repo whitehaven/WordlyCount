ff9ddda1366b937b3d876cc83d494d4985ade5c5
whitehaven <accrist@me.com>
2014-08-27 22:49:35 -0600

    Initial Commit

---
+++ WordlyCount/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WordlyCount
+//
+//  Created by Alex Crist on 8/27/14.
+//  Copyright (c) 2014 White Haven Informatics. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
